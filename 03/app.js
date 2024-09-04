e40ef758c9c6bdf746459527837b2b1515f8527e
sauravztx <saurabh.ztx@gmail.com>
2024-09-04 23:13:26 +0530

    MonogDb Basics & CRUD

---
+++ 03/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const userModel = require('./usermodel');
+
+app.get("/", function(req, res){
+    res.send("working")
+})
+
+app.get('/create', async (req, res) => {
+    let createdUser = await userModel.create({
+        name: "Neo Thomas",
+        email : "Neo.ztx@gmail.com",
+        username: "neo"
+    })
+    res.send(createdUser);
+})
+
+app.get('/update', async (req, res) => {
+    const updatedUser = await userModel.findOneAndUpdate({username: "saurav"}, {name : "Alex"}, {new : true})
+    res.send(updatedUser);
+})
+
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    res.send(users);
+})
+
+app.get('/delete', async (req, res) => {
+    let users = await userModel.findOneAndDelete({username: 'neo'});
+    res.send(users);
+})
+
+
+
+
+app.listen(3000);
